9f5385dc8a23e81637d93cbd53823d64bf7a5eb6
anshulm07 <anshulmittal07@gmail.com>
2022-12-02 16:12:12 -0500

    Completed issue GH-5

--- backend/server.js
+++ backend/server.js
@@ -24,7 +24,179 @@ mongoose
   .catch((err) => console.log(err));
 // 
 
+app.post("/newAccount", (req, res) => {
+  User.findOne({ email: req.body.email.toLowerCase() }).then((user) => {
+      if (user) {
+      return res.status(400).json({ email: "A user account with that email already exists." });
+      } else {
+          const newAccount = new User({
+              _id: new mongoose.Types.ObjectId(),
+              accountType: req.body.accountType,
+              name: req.body.name,
+              email: req.body.email.toLowerCase(),
+              createdAt: Date(),
+              professorInfo: req.body.professorInfo,
+              studentInfo: null,
+              adminInfo: null,
+          });
 
+          const course1 = new Course({
+              _id: new mongoose.Types.ObjectId(),
+              name: "CS 4641",
+              professorInfo: newAccount._id,
+              application: null,
+          });
+          
+          const app1 = new Application({
+              _id: new mongoose.Types.ObjectId(),
+              name: "CS 4641 Application",
+              professorId: newAccount._id,
+              default: true,
+              description: "This is the default application for CS 4641",
+              assignedCourse: course1._id,
+              questions: [
+                  {
+                      written: false,
+                      questionPrompt: "What is your GPA?",
+                      options: [
+                          {
+                              optionText: "< 3.0",
+                          },
+                          {
+                              optionText: "3.0 - 3.5",
+                          },
+                          {
+                              optionText: "> 3.5",
+                          },
+                      ],
+                      response: "",
+                  },
+                  {
+                      written: true,
+                      questionPrompt: "Why do you want to join this course?",
+                      options: [],
+                      response: "",
+                  },
+              ],
+          });
+          app1.save().catch((err) => console.log(err));
+          course1.application = app1._id;
+          course1
+          .save()
+          .catch((err) => console.log(err));
+
+          const course2 = new Course({
+              _id: new mongoose.Types.ObjectId(),
+              name: "CS 7641",
+              professorInfo: newAccount._id,
+              application: null,
+          });
+          const app2 = new Application({
+              _id: new mongoose.Types.ObjectId(),
+              name: "CS 7641 Application",
+              professorId: newAccount._id,
+              default: true,
+              description: "This is the default application for CS 7641",
+              assignedCourse: null,
+              questions: [
+                  {
+                      written: false,
+                      questionPrompt: "What is your GPA?",
+                      options: [
+                          {
+                              optionText: "< 3.0",
+                          },
+                          {
+                              optionText: "3.0 - 3.5",
+                          },
+                          {
+                              optionText: "> 3.5",
+                          },
+                      ],
+                      response: "",
+                  },
+                  {
+                      written: true,
+                      questionPrompt: "Why do you want to join this course?",
+                      options: [],
+                      response: "",
+                  },
+              ],
+          });
+          app2.save().catch((err) => console.log(err));
+          course2.application = app2._id;
+          course2
+          .save()
+          .catch((err) => console.log(err));
+
+          newAccount.professorInfo.courses = {"CS4641": course1._id, "CS7641": course2._id};
+          newAccount
+          .save()
+          .then((newAccount) => {
+              console.log(newAccount);
+              res.json("Account for " + newAccount.name + " has been created! Their ID is " + newAccount._id + " \n Their courses are: " + newAccount.professorInfo.courses["CS4641"] + " and " + newAccount.professorInfo.courses["CS7641"]);
+          })
+          .catch((err) => console.log(err));
+      }
+  });
+});
+
+app.post("/addNewApplication", (req, res) => {
+  const newApplication = new Application({
+      _id: new mongoose.Types.ObjectId(),
+      name: req.body.name,
+      professorId:  mongoose.Types.ObjectId(req.body.professorId),
+      default: false,
+      description: req.body.description,
+      assignedCourse: req.body.assignedCourse,
+      questions: req.body.questions,
+  });
+  newApplication
+  .save()
+  .then((newApplication) => {
+      console.log(newApplication);
+      res.json(newApplication.name + " has been created by " + newApplication.professorId + "! \n The application ID is " + newApplication._id);
+  })
+  .catch((err) => console.log(err));
+});
+
+app.post("/assignApplicationToCourse", (req, res) => {
+  console.log(req.body.courseId)
+  Course
+  .findById
+  (mongoose.Types.ObjectId(req.body.courseId))
+  .then((course) => {
+      console.log(course)
+      course.application =mongoose.Types.ObjectId(req.body.applicationId);
+      course
+      .save()
+      .then((course) => {
+          console.log(course);
+          res.json("Application " + course.application + " has been assigned to course " + course.name + "!");
+      })
+      .catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
+});
+
+app.post("/editApplication", (req, res) => {
+  Application
+  .findById
+  (req.body.applicationId)
+  .then((application) => {
+      application.name = req.body.name;
+      application.description = req.body.description;
+      application.questions = req.body.questions;
+      application
+      .save()
+      .then((application) => {
+          console.log(application);
+          res.json("Application " + application.name + " has been updated!");
+      })
+      .catch((err) => console.log(err));
+  })
+  .catch((err) => console.log(err));
+});
 
 app.listen(3000, () => console.log('TA Application Server is listening on port 3000.'));
 

ffa4e1f8547b4c628d68d1aef62a41443dc963a3
Ritvik Chandrashekhar <rchandra38@gatech.edu>
2022-12-02 16:22:26 -0500

    fixed issue GH-6

---
+++ backend/models/Application.js
@@ -0,0 +1,43 @@
+var mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+var ApplicationSchema = new mongoose.Schema({
+
+  name: {
+    type: String,
+    defualt: "default"
+    },
+    professorId: {
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+    },
+
+    default: {
+        type: Boolean, 
+        default: true
+    },
+
+  description: {
+        type: String,
+        default: ""
+    },
+
+  assignedCourse: 
+    { type: Schema.Types.ObjectId, ref: "Course" },
+
+  questions : [{
+    _id: 0,
+    written: {type: Boolean, default: false},
+    questionPrompt: String,
+    options: [{
+      optionText : String,
+      _id: 0
+    }],
+    response: {type: String, default: ""},
+    }],
+
+ }, {timestamps: true});
+
+Application = mongoose.model('Application', ApplicationSchema);
+
+module.exports = Application; 

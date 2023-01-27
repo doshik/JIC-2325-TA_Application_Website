81aa668f7d550bca6a2c44947bef9b6a95f9d58d
doshik <kabirdoshi@gmail.com>
2023-01-26 21:54:18 -0500

    routing changes

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,12 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import BaseRouter from "./routes/router";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/css/bootstrap.css";
+import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <RouterProvider router={BaseRouter} />
-  </React.StrictMode>
+    <App/>
 );
--- frontend/ta_application/src/routes/scenes/ProfCourseApplicationContainer.js
+++
@@ -1,8 +0,0 @@
-import * as React from "react";
-import ProfCourseApplicationView from "../../components/application/ProfCourseApplicationView";
-
-const ProfCourseApplicationContainer = () => {
-  return <ProfCourseApplicationView />;
-};
-
-export default ProfCourseApplicationContainer;

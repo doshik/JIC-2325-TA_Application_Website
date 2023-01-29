e23ca3c450989e66fadc96ec73ea1d55ed6003fa
AritroB <77176367+AritroB@users.noreply.github.com>
2023-01-29 00:57:24 -0500

    Created default application form

--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {Route, Routes, Redirect } from "react-router-dom";
 import ProfCourseApplicationView from "../pages/application/ProfCourseApplicationView";
+import DefaultApplicationFormView from "../pages/application_form/DefaultApplicationFormView";
 
 
 const Routing = () => (
       <Routes>
         <Route exact path="/" element={<ProfCourseApplicationView/>} />
+        <Route exact path="/prof/applicationtemplates/defaultform" element={<DefaultApplicationFormView/>} />
       </Routes>
   );
   

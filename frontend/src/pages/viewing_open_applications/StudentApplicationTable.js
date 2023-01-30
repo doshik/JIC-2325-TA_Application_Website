ca2195116bcdae89f98f5e222824196370eef168
Ritvik Chandrashekhar <ritvik.c.27@gmail.com>
2023-01-29 22:45:26 -0500

    Added student view of open applications

---
+++ frontend/src/pages/viewing_open_applications/StudentApplicationTable.js
@@ -0,0 +1,62 @@
+import * as React from "react";
+import { Table, Button, Container } from "react-bootstrap";
+
+const StudentApplicationTable = () => {
+  return (
+        <Container className="mt-3">
+            <Table hover size="sm">
+                <thead>
+                    <tr>
+                        <th scope="col">Course Name</th>
+                        <th scope="col">CRN</th>
+                        <th scope="col">Professor Name</th>
+                        <th scope="col">Semester</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>CS 1332</td>
+                        <td>54321</td>
+                        <td>Richard Landry</td>
+                        <td>Spring 2023</td>
+                        <td>
+                        <Button>View</Button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>CS 2050</td>
+                        <td>12345</td>
+                        <td>Gerandy Brito</td>
+                        <td>Spring 2023</td>
+                        <td>
+                        <Button>View</Button>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>CS 4641</td>
+                        <td>54321</td>
+                        <td>Richard Landry</td>
+                        <td>Spring 2023</td>
+                        <td>
+                        <Button>View</Button>
+                        </td>
+                        
+                    </tr>
+                    <tr>
+                        <td>CS 3311</td>
+                        <td>25299</td>
+                        <td>Rodrigo Valente Borela</td>
+                        <td>Spring 2023</td>
+                        <td>
+                        <Button>View</Button>
+                        </td>
+                    </tr>
+                </tbody>
+            </Table>
+            <Button variant="secondary">Go Back</Button>{' '}
+        </Container>
+    );
+};
+
+export default StudentApplicationTable;
--- frontend/src/routes/Router.js
+++ frontend/src/routes/Router.js
@@ -1,11 +1,13 @@
 import React from "react";
 import {Route, Routes, Redirect } from "react-router-dom";
 import ProfCourseApplicationView from "../pages/application/ProfCourseApplicationView";
+import StudentApplicationsView from "../pages/viewing_open_applications/StudentApplicationsView";
 
 
 const Routing = () => (
       <Routes>
         <Route exact path="/" element={<ProfCourseApplicationView/>} />
+        <Route exact path= "/student/openapplications" element={<StudentApplicationsView/>} />
       </Routes>
   );
   

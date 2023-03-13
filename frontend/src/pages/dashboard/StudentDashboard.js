201a8d534d07ec785391ebce2b144d9813223b07
AritroB <77176367+AritroB@users.noreply.github.com>
2023-03-12 21:57:07 -0400

    Update StudentDashboard.js

--- frontend/src/pages/dashboard/StudentDashboard.js
+++ frontend/src/pages/dashboard/StudentDashboard.js
@@ -2,25 +2,53 @@ import * as React from "react";
 import { Button, Card, Row, Col, Container } from "react-bootstrap";
 
 const StudentDashboard = () => {
-  const Courses = ["CS 1331", "CS 1332", "CS 3510", "CS 4641"];
+  const Courses = [
+    { courseName: 'CS 1332', progress: "In Progress", professor: "Olufisayo Omojokun" },
+    { courseName: 'CS 2340', progress: "In Progress", professor: "Aibek Musaev" },
+    { courseName: 'CS 4641', progress: "Submitted", professor: "Mahdi Roozbahani" },
+    { courseName: 'CS 2110', progress: "Submitted", professor: "Caleb Southern"}
+  ];
+
+  const inProgressCourses = Courses.filter(course => course.progress === "In Progress");
+  const submittedCourses = Courses.filter(course => course.progress === "Submitted");
 
   return (
     <Container>
-      <Row xs={1} md={2}>
-        {Courses.map((Course, idx) => (
-          <Col>
-            <Card style={{ marginTop: "15px" }}>
-              <Card.Header as="h5">{Course}</Card.Header>
+      <Row>
+        <Col>
+          <h5>In Progress</h5>
+          {inProgressCourses.map((course, index) => (
+            <Card key={index} className="mb-3">
               <Card.Body>
-                <div className="text-center">
-                  <Button style={{ width: "90%" }} variant="primary">
-                    Status: Pending Review
-                  </Button>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div>
+                    <Card.Title>{course.courseName}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{course.professor}</Card.Subtitle>
+                  </div>
+                  <Button variant="primary">View/Edit</Button>
                 </div>
               </Card.Body>
             </Card>
-          </Col>
-        ))}
+          ))}
+        </Col>
+      </Row>
+      <Row>
+        <Col>
+          <h5>Submitted</h5>
+          {submittedCourses.map((course, index) => (
+            <Card key={index} className="mb-3">
+              <Card.Body>
+                <div className="d-flex justify-content-between align-items-center">
+                  <div>
+                    <Card.Title>{course.courseName}</Card.Title>
+                    <Card.Subtitle className="mb-2 text-muted">{course.professor}</Card.Subtitle>
+                  </div>
+                  <Button variant="primary">View Submission</Button>
+                </div>
+              </Card.Body>
+            </Card>
+          ))}
+        </Col>
       </Row>
     </Container>
   );

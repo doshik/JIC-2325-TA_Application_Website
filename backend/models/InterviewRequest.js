c6ecde0a46def532b63716e47b4a1ac831a235d7
Nithik Balachandran <nithikbala@gmail.com>
2023-03-12 21:42:05 -0400

    base model

---
+++ backend/models/InterviewRequest.js
@@ -0,0 +1,32 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const InterviewRequest = new Schema({
+  student: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  professor: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+  },
+  application: {
+    type: Schema.Types.ObjectId,
+    ref: "Application",
+  },
+  possibleTimes: {
+    type: Array,
+    default: [],
+  },
+  acceptedTime: {
+    type: String,
+    default: "",
+  },
+  meetingLink: {
+    type: String,
+    default: "",
+  },
+});
+
+InterviewRequest = mongoose.model("InterviewRequest", interviewRequestSchema);
+module.exports = InterviewRequest;

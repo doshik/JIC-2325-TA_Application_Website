ee3b0f50c5fc0f1bae81c2db04322a7f0af7914b
doshik <kabirdoshi@gmail.com>
2023-03-13 18:47:03 -0400

    fixed env var issue

--- frontend/src/api/main.js
+++ frontend/src/api/main.js
@@ -1,12 +1,10 @@
 import axios from "axios";
 axios.defaults.withCredentials = true;
 
-const API_BASE_URL = "http://localhost:5001";
-
 export const get = async (url) => {
-  console.log(`${API_BASE_URL}${url}`);
+  console.log(`${process.env.REACT_APP_API_URL}${url}`);
   try {
-    const response = await axios.get(`${API_BASE_URL}${url}`);
+    const response = await axios.get(`${process.env.REACT_APP_API_URL}${url}`);
     return response;
   } catch (err) {
     throw err;
@@ -16,10 +14,10 @@ export const get = async (url) => {
 export const post = async (url, data, headers = null) => {
   const config = {
     method: "post",
-    url: `${API_BASE_URL}${url}`,
+    url: `${process.env.REACT_APP_API_URL}${url}`,
     data: data,
   };
-  console.log(`${API_BASE_URL}${url}`, data);
+  console.log(`${process.env.REACT_APP_API_URL}${url}`, data);
   try {
     const response = await axios(config);
     return response;
@@ -29,9 +27,9 @@ export const post = async (url, data, headers = null) => {
 };
 
 export const put = async (url, data) => {
-  return await axios.put(`${API_BASE_URL}${url}`, data);
+  return await axios.put(`${process.env.REACT_APP_API_URL}${url}`, data);
 };
 
 export const del = async (url) => {
-  return await axios.delete(`${API_BASE_URL}${url}`);
+  return await axios.delete(`${process.env.REACT_APP_API_URL}${url}`);
 };

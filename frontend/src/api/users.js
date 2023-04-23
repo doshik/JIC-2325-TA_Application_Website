05dec77755632199f31efce084cd3d640409a784
doshik <kabirdoshi@gmail.com>
2023-04-23 19:49:34 -0400

    set cookie

--- frontend/src/api/users.js
+++ frontend/src/api/users.js
@@ -1,18 +1,19 @@
 import { get, post } from "./main";
+import Cookies from "js-cookie";
 
 // a function to log the user in using the api const above
 export const login = async (role) => {
   console.log("login called");
   const response = await post(`/user/login`, {
     role: role,
-  }, {
-    withCredentials: true
   }).catch((err) => {
     throw err;
   });
+  if (response.data.loggedIn) {
+    Cookies.set('jwt', response.data.token);
+  };
   return response.data;
 };
-
 
 // a function to check if the user is logged in using the api const above
 export const isLoggedIn = async () => {

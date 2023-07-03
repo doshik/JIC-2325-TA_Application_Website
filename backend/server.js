d7e121bffbe888b8d43d8279e2e8174b837cf698
jrgood01 <jacob.r.haygood@gmail.com>
2023-07-03 18:48:13 -0400

    modify server.js to comply with plesk

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,17 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookies = require("cookie-parser");
 const cors = require("cors");
+const path = require('path');
+
+const PORT = process.env.PORT || 80;
+
 require("dotenv").config();
 
 var app = express();
 app.use(cookies());
 
 var allowedOrigins = [
+  "http://127.0.0.1",
   "http://127.0.0.1:3000",
   "http://localhost:3000",
   "http://localhost:3001",
@@ -68,6 +73,17 @@ app.use("/interview", interviewRoutes);
 courseRoutes = require("./controllers/Course.controller");
 app.use("/course", courseRoutes);
 
+// Serve static files from the React app
+app.use(express.static(path.join(__dirname, '../frontend/build')));
+
+// The "catchall" handler: for any request that doesn't
+// match one above, send back React's index.html file.
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend/build/index.html'));
+});
+
+
+
 // const auth = require("./routes/auth");
 // app.use("/auth", auth);
 
@@ -89,7 +105,6 @@ const connectDB = async () => {
   }
 };
 
-const PORT = process.env.PORT || 5000;
 //Connect to the database before listening
 connectDB().then(() => {
   app.listen(PORT, () => {
